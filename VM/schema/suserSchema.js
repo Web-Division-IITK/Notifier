c708c10e8c675f2d0d25bab012d08d9f24ee2685
Utkarsh Gupta <utkarshg99@gmail.com>
2020-10-26 17:47:08 +0530

    BFx 2.0.2.1

--- VM/schema/suserSchema.js
+++ VM/schema/suserSchema.js
@@ -6,7 +6,7 @@ var suser = new Schema({
     uid: {type:String, required:true, unique: true},
     id: {type:String, required:true, unique: true},
     email: {type:String, required:true},
-    name: {type:String, required:true},
+    name: {type:String, required:false},
     rollno: {type:String, required:false},
     admin: {type:Boolean, default:false},
     council: {type:Schema.Types.Mixed, default: vals}
